b699da418f2d177c527c7b007d46cbeafd780d14
ganyue <ganyue126@126.com>
2016-08-08 17:03:16 +0800

    1、使沉浸式可选

--- MapSocial/appbase/src/main/java/com/gy/appbase/activity/BaseFragmentActivity.java
+++ MapSocial/appbase/src/main/java/com/gy/appbase/activity/BaseFragmentActivity.java
@@ -15,6 +15,12 @@ import com.gy.appbase.inject.ViewInjectInterpreter;
  */
 public abstract class BaseFragmentActivity extends FragmentActivity {
     protected BaseFragmentActivityController mController;
+    private boolean transluentStatusAndNavigation = false;
+
+    protected void setTransluentStatusAndNavigation (boolean transluentStatusAndNavigation) {
+        this.transluentStatusAndNavigation = transluentStatusAndNavigation;
+    }
+
     public void setController (BaseFragmentActivityController controller) {
         mController = controller;
     }
@@ -27,8 +33,18 @@ public abstract class BaseFragmentActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //android4.4.4 (sdk 19) 以后可以实现沉浸式
-        if (Build.VERSION.SDK_INT >= 19) {
+        if (mController == null) {
+            instanceController();
+        }
+
+        setContent(savedInstanceState);
+        findViews(savedInstanceState);
+        initViews(savedInstanceState);
+
+        /**
+         * android4.4.4 (sdk 19) 以后可以实现沉浸式
+         */
+        if (transluentStatusAndNavigation && Build.VERSION.SDK_INT >= 19) {
             /**
              * 这里只是这样的话会导致view被拉上去，要避免的话需要在布局文件里头加入下面两句
              * android:clipToPadding="false"     //绘制到padding区域
@@ -37,14 +53,6 @@ public abstract class BaseFragmentActivity extends FragmentActivity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
-
-        if (mController == null) {
-            instanceController();
-        }
-
-        setContent(savedInstanceState);
-        findViews(savedInstanceState);
-        initViews(savedInstanceState);
     }
 
     @Override
